6669180b1850d780742244b8d9856f202ce3519d
Krish18 <krishnataur018@gmail.com>
2025-12-05 19:18:54 +0530

    Added input-output programs

---
+++ 01_Python/03-input-output/01-user_input.py
@@ -0,0 +1,6 @@
+a = input("enter value of a: ")
+print (a)
+
+name = input("enter name: ")
+print("Welcome", name)
+
---
+++ 01_Python/03-input-output/03-average.py
@@ -0,0 +1,6 @@
+a = float(input("enter 1st no:"))
+b = float(input("enter 2nd no:"))
+
+avg = (a+b)/2
+
+print("Average: ", avg)
