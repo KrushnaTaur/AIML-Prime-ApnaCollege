b0d5159d198ea8e1e6d721f9b333e13fb7b04eff
Krish18 <krishnataur018@gmail.com>
2025-12-15 19:16:07 +0530

    Added classes and objects programs

---
+++ 01_Python/07-oops/01-class_objects.py
@@ -0,0 +1,10 @@
+class Student:   #class
+    subject = "Python"
+    college = "MIT"
+    year = "3rd Year"
+
+stud1 = Student()   #object
+stud2 = Student()
+
+print(stud1.subject, stud1.college, stud1.year)
+print(stud2.subject, stud2.college, stud2.year)
