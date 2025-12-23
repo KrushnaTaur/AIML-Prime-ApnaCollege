cbc0b063a4a5a33af575ab404be136fe7c0bbcc5
Krish18 <krishnataur018@gmail.com>
2025-12-23 09:06:47 +0530

    Added list comprehension examples

---
+++ 01_Python/06-data-structures/14-list_comprehension.py
@@ -0,0 +1,13 @@
+sq = [i*i for i in range(6)]
+print(sq)
+
+sq = [i*i for i in range(6) if i % 2 !=0]
+print(sq)
+
+nums = [-2, -3, 3, 4, -1, 7]
+nums = [0 if val < 0 else val for val in nums]
+print(nums)
+
+words = ["hello", "python", "krushna"]
+words = [val.upper() for val in words]
+print(words)
