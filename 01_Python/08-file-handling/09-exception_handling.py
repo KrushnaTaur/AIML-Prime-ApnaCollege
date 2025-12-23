ee071dce499d7d7202e8fcfb7ba53724618130bc
Krish18 <krishnataur018@gmail.com>
2025-12-23 08:55:49 +0530

    Added exception handling and finally keyword examples

---
+++ 01_Python/08-file-handling/09-exception_handling.py
@@ -0,0 +1,15 @@
+try:
+    x = int(input("Enter x: "))
+    ans = 10/x
+
+except ZeroDivisionError:
+    print("Divide by 0 is not allowed")
+
+except ValueError:
+    print(f"Invalid input")
+
+else:
+    print(f"ans = {ans}")
+
+finally:
+    print("End of the program")
